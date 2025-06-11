f627313f53f68e26d1c3157097f7cf37a64204fc
Daniil Kurganov <dk30128@gmail.com>
2025-06-11 23:27:26 +0300

    Implemented skeleton of round stepper (draft)

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,23 +1,33 @@
-import {Component} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import {Component, inject} from "@angular/core";
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
      
 @Component({
     selector: "web-authentication",
     standalone: true,
-    imports: [FormsModule, HttpClientModule, MatCardModule, MatInputModule, MatButtonModule],
+    imports: [FormsModule, HttpClientModule, MatCardModule, MatInputModule, MatButtonModule, MatStepperModule, MatFormFieldModule, ReactiveFormsModule],
     templateUrl: "app.component.html"
 })
 export class AppComponent {
   p: number;
   g: number;
   y: number;
+  x: number;
   numberSessionRounds = "-";
   numberDoneRounds = "-";
   numberSuccessRounds = "-";
+  private _formBuilder = inject(FormBuilder);
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
   constructor(private http: HttpClient){}
 
